0c699f59f0d9abc90c226721925800e1ae453c4a
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2024-02-16 22:37:30 +0000

    ホームを作成
    WIP

--- tailwind-application.config.js
+++ tailwind-application.config.js
@@ -12,6 +12,18 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'corporate-50': '#0ab360',
+        'corporate-100': '#b0ffcd',
+        'corporate-200': '#80ffb3',
+        'corporate-300': '#50ff9b',
+        'corporate-400': '#28ff88',
+        'corporate-500': '#18e676',
+        'corporate-600': '#0ab360',
+        'corporate-700': '#008048',
+        'corporate-800': '#004d25',
+        'corporate-900': '#001b07',
+      },
     },
   },
   plugins: [
