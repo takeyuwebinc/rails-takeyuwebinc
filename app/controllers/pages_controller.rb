bdbc46d08c23f057e23df90242dab198cbb95f1d
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2024-02-16 22:44:24 +0000

    キャッシュを有効にする
    CloudFrontのエッジキャッシュを活用する

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
+  before_action :set_cache_control
+
   def index
   end
+
+  private
+
+    def set_cache_control
+      request.session_options[:skip] = true
+      expires_in 1.hour, public: true
+    end
 end
