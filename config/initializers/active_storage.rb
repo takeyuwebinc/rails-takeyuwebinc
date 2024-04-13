ecebb2f94f6f77fa5fbb1fd094f936eeaa95b22d
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2024-04-13 16:40:58 +0000

    ActiveAdminで ActiveStorage::Blob を確認できるようにする

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -4,4 +4,10 @@ Rails.application.reloader.to_prepare do
       expires_in ActiveStorage.service_urls_expire_in, public: true
     end
   end
+
+  class ActiveStorage::Blob
+    def self.ransackable_attributes(auth_object = nil)
+      [ "byte_size", "checksum", "content_type", "created_at", "filename", "id", "id_value", "key", "metadata", "service_name" ]
+    end
+  end
 end
