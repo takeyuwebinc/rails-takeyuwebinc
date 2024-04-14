e93fd1a66b1eeae0466e7130cdcd74716dc4779a
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2024-04-14 06:00:44 +0000

    アップロードファイルのバリデーションを追加
    - 1ファイル当たり10MB
    - JPEG, PNG, GIF, PDF, ZIP, TXT のみ

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -6,6 +6,9 @@ Rails.application.reloader.to_prepare do
   end
 
   class ActiveStorage::Blob
+    validates :byte_size, numericality: { less_than: 10.megabytes }, if: :byte_size_changed?
+    validates :content_type, allow_nil: true, inclusion: { in: %w[image/png image/jpg image/jpeg image/gif application/x-zip-compressed application/pdf text/plain] }, if: :content_type_changed?
+
     def self.ransackable_attributes(auth_object = nil)
       [ "byte_size", "checksum", "content_type", "created_at", "filename", "id", "id_value", "key", "metadata", "service_name" ]
     end
