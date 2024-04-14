e05f51077df240cd9b815326a12ed0aaab0e6664
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2024-04-14 08:47:19 +0000

    allow webp
    画像をwebpに変換後保存できる

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -7,7 +7,7 @@ Rails.application.reloader.to_prepare do
 
   class ActiveStorage::Blob
     validates :byte_size, numericality: { less_than: 10.megabytes }, if: :byte_size_changed?
-    validates :content_type, allow_nil: true, inclusion: { in: %w[image/png image/jpg image/jpeg image/gif application/x-zip-compressed application/pdf text/plain] }, if: :content_type_changed?
+    validates :content_type, allow_nil: true, inclusion: { in: %w[image/png image/jpg image/jpeg image/gif image/webp application/x-zip-compressed application/pdf text/plain] }, if: :content_type_changed?
 
     def self.ransackable_attributes(auth_object = nil)
       [ "byte_size", "checksum", "content_type", "created_at", "filename", "id", "id_value", "key", "metadata", "service_name" ]
