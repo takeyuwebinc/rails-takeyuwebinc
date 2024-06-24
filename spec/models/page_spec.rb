7243e12cd000a0ff0d4a0abcba189d7032c20670
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2024-06-24 06:32:51 +0000

    fix a broken spec

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -31,9 +31,8 @@ RSpec.describe Page, type: :model do
     it do
       page = create(:page, path: "/", markdown: "# Hello")
       built_html = page.to_html
-      expect(built_html).to include(%Q(<div class="text-gray-900 dark:text-gray-100">))
-      expect(built_html).to include(%Q(<h1 class="text-3xl font-bold mb-2 border-b-2 border-gray-300 dark:border-gray-700 pb-2">Hello</h1>))
-      expect(built_html).to include(%Q(! tailwindcss))
+      expect(built_html).to include(%Q(<div class="w-full pt-10 pb-20 px-5 sm:px-10">))
+      expect(built_html).to include(%Q(<div class="page text-gray-900 dark:text-gray-100"><h1 class="text-3xl font-bold mb-2 border-b-2 border-gray-300 dark:border-gray-700 pb-2">Hello</h1></div>))
     end
 
     it do
