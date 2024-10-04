1b64a3183f7a126afb5ba7d4168a0b6d466a79d4
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2024-10-04 05:10:56 +0000

    fix:  bin/rails app:update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_08_080446) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_04_050612) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
