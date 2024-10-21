438ece78c3feab2a4a913eca4b1b9f619e1fc721
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2024-10-21 02:56:55 +0000

    fix: お知らせ

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,9 @@
 class Work < ApplicationRecord
   belongs_to :client
-  has_one_attached :image
+  has_one_attached :image do |attachable|
+    attachable.variant :default, { resize_to_limit: [ 1024, 1024 ], convert: :webp, saver: { subsample_mode: "on", strip: true, interlace: true, quality: 80 } }
+    attachable.variant :thumb, { resize_to_limit: [ 360, 360 ], convert: :webp, saver: { subsample_mode: "on", strip: true, interlace: true, quality: 80 } }
+  end
   has_rich_text :description
   has_rich_text :content
   validates :slug, presence: true, uniqueness: true, format: { with: /\A[a-z0-9-]+\z/ }
--- app/models/work_style.rb
+++ app/models/work_style.rb
@@ -1,5 +1,8 @@
 class WorkStyle < ApplicationRecord
-  has_one_attached :image
+  has_one_attached :image do |attachable|
+    attachable.variant :default, { resize_to_limit: [ 1024, 1024 ], convert: :webp, saver: { subsample_mode: "on", strip: true, interlace: true, quality: 80 } }
+    attachable.variant :thumb, { resize_to_limit: [ 360, 360 ], convert: :webp, saver: { subsample_mode: "on", strip: true, interlace: true, quality: 80 } }
+  end
   has_rich_text :description
   has_rich_text :content
   validates :slug, presence: true, uniqueness: true, format: { with: /\A[a-z0-9-]+\z/ }
